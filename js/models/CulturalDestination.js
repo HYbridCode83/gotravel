e758b7b084e11e5f4bc60cbc45fa36b622cbe0b8
HYbridCode83 <bcs24020040@student.uts.edu.my>
2025-05-04 15:03:29 +0800

    Create CulturalDestination.js

---
+++ js/models/CulturalDestination.js
@@ -0,0 +1,29 @@
+import Destination from './Destination';
+
+class CulturalDestination extends Destination {
+    constructor(firebaseData) {
+        super(firebaseData);
+        this.culture = firebaseData.culture || '';
+        this.traditions = firebaseData.traditions || [];
+        this.events = firebaseData.events || [];
+    }
+
+    toFirebaseObject() {
+        return {
+            ...super.toFirebaseObject(),
+            culture: this.culture,
+            traditions: this.traditions,
+            events: this.events
+        };
+    }
+
+    addEvent(event) {
+        this.events.push(event);
+    }
+
+    addTradition(tradition) {
+        this.traditions.push(tradition);
+    }
+}
+
+export default CulturalDestination;
