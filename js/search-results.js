71b1f3a977c4f6cd88f71443bdbbee5a1498044b
HYbridCode83 <bcs24020040@student.uts.edu.my>
2025-05-04 16:29:09 +0800

    Update search-results.js

--- js/search-results.js
+++ js/search-results.js
@@ -1,13 +1,6 @@
 // Import Firebase functions
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 import { db } from './firebase-init.js';
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-import DestinationFactory from './models/DestinationFactory.js';
-import DestinationFactory from './models/DestinationFactory.js';
-import { HistoricalDestination } from './models/HistoricalDestination.js';
-import { NatureDestination } from './models/NatureDestination.js';
-import { CulturalDestination } from './models/CulturalDestination.js';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
